61e1e7477c091f78cd9d50bbafaca41fbe7b62be
LeiXuewei <leixuewei@oneapm.com>
2017-01-04 18:03:34 +0800

    resettle code.
    Signed-off-by: LeiXuewei <leixuewei@oneapm.com>

--- index.js
+++ index.js
@@ -1,151 +1,38 @@
-
-var PENDING = 'PENDING'
-var FULFILLED = 'FULFILLED'
-var REJECTED = 'REJECTED'
-
-function QueueItem (promise, onSuccess, onError) {
-  if (!(this instanceof QueueItem)) {
-    return new QueueItem(promise, onSuccess, onError)
-  }
-
-  this.$promise = promise
-  this.$onSuccess = onSuccess && typeof onSuccess === 'function' ? onSuccess : function (item) { return item }
-  this.$onError = onError && typeof onError === 'function' ? onError : function (item) { throw item }
-}
-
-QueueItem.prototype.FULFILLED = function(value) {
-  execCallback.call(this.$promise, this.$onSuccess, value)
-}
-
-QueueItem.prototype.REJECTED = function(value) {
-  execCallback.call(this.$promise, this.$onError, value)
-}
+var PromiseBase = require('./core')
 
 function Promise(resolver) {
-  if (!(this instanceof Promise)) {
+  if(!(this instanceof Promise)) {
     return new Promise(resolver)
   }
 
-  this.$status = PENDING
-  this.$value = undefined
-  this.$queue = []
-
-  if (resolver) {
-    execResolver.call(this, resolver)
-  }
-}
-module.exports = Promise
-
-function execResolver(resolver) {
-  var self = this
-    , isCalled = false
-
-  function onSuccess(value) {
-    if (isCalled)
-      return
-    isCalled = true
-
-    execQueue.call(self, FULFILLED, value)
-  }
-
-  function onError(reason) {
-    if (isCalled)
-      return
-    isCalled = true
-
-    execQueue.call(self, REJECTED, reason)
-  }
-
-  try {
-    resolver(onSuccess, onError)
-  } catch (e) {
-    onError(e)
-  }
+  this.$base = new PromiseBase(resolver)
 }
 
-function getThenMethod(value) {
-  var then = value && value.then
-  if (value && typeof value === 'object' && typeof then === 'function') {
-    return function (resolve, reject) {
-      then.call(value, resolve, reject)
-    }
-  } else {
-    return null
-  }
-}
-
-function execQueue(type, value) {
-  if (type === FULFILLED && (typeof value === 'object' || typeof value === 'function')) {
-    var then = null
-
-    try {
-      then = getThenMethod(value)
-    } catch (e) {
-      execQueue.call(this, REJECTED, e)
-
-      return
-    }
-
-    if (then) {
-      execResolver.call(this, then)
-
-      return
-    }
-  }
-
-  this.$status = type
-  this.$value = value
-
-  for (var index = 0; index < this.$queue.length; index++) {
-    var item = this.$queue[index];
-    if (item) {
-      item[type](value)
-    }
-  }
-}
-
-function execCallback(func, value) {
-  var self = this
-
-  setTimeout(function() {
-    var result = null
-    try {
-      result = func(value)
-    } catch (e) {
-      execQueue.call(self, REJECTED, e)
-
-      return
-    }
-
-    if (result === self) {
-      execQueue.call(self, REJECTED, new TypeError('Can not return itself.'))
-    } else {
-      execQueue.call(self, FULFILLED, result)
-    }
-  }, 4)
-}
-
-Promise.prototype.then = function (onSuccess, onError) {
-  if (typeof onSuccess !== 'function' && this.$status === FULFILLED ||
-      typeof onError !== 'function' && this.$status === REJECTED) {
-    return this
-  }
-
-  var promise = new Promise()
-
-  if (this.$status === PENDING) {
-    this.$queue.push(new QueueItem(promise, onSuccess, onError))
-  } else if (this.$status === FULFILLED) {
-    execCallback.call(promise, onSuccess, this.$value)
-  } else {
-    execCallback.call(promise, onError, this.$value)
-  }
-
-  return promise
+Promise.prototype.then = function (resolve, reject) {
+  return this.$base.then(resolve, reject)
 }
 
 Promise.prototype.catch = function (onError) {
   return this.then(null, onError)
+}
+
+Promise.prototype.wait = function (tick) {
+  return this.then(
+    function (value) {
+      return new Promise(function(resolve, reject) {
+        setTimeout(function() {
+          resolve(value)
+        }, tick)
+      })
+    },
+    function (reason) {
+      return new Promise(function(resolve, reject) {
+        setTimeout(function() {
+          reject(reason)
+        }, tick)
+      })
+    }
+  )
 }
 
 Promise.deferred = Promise.defer = function () {
@@ -184,17 +71,6 @@ Promise.sequence = function (funcs) {
   }
 
   return promise
-}
-
-function filter(data, callback) {
-  var result = []
-  for (var index = 0; index < data.length; index++) {
-    var item = data[index]
-    if (callback(item, index)) {
-      result.push(item)
-    }
-  }
-  return result
 }
 
 Promise.all = function (promises) {
@@ -281,21 +157,4 @@ Promise.timeout = function (promise, tick) {
   ])
 }
 
-Promise.prototype.wait = function (tick) {
-  return this.then(
-    function (value) {
-      return new Promise(function(resolve, reject) {
-        setTimeout(function() {
-          resolve(value)
-        }, tick)
-      })
-    },
-    function (reason) {
-      return new Promise(function(resolve, reject) {
-        setTimeout(function() {
-          reject(reason)
-        }, tick)
-      })
-    }
-  )
-}
+module.exports = Promise
